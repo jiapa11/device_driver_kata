0c8224db804f16cc7f3fb8e79aeb74bdcdb1160e
noname <none@none.com>
2025-07-03 16:21:48 +0900

    Implemented device driver with unit test and mocking

--- device_driver_kata/device_driver.cpp
+++ device_driver_kata/device_driver.cpp
@@ -5,12 +5,47 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice *hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
+    int readValue = read_(address);
+    if (false == verifyRead(readValue, address)) {
+        throw ReadException();
+    }
+    return readValue;
+}
+
+int DeviceDriver::read_(long address)
+{
     return (int)(m_hardware->read(address));
+}
+
+bool DeviceDriver::verifyRead(int readValue, long address)
+{
+    for (int i = 0; i < 4; i++) {
+        if (readValue != read_(address)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
+    int readValue = read_(address);
+    if (FlashMemoryDevice::DELETED == readValue)
+    {
+        write_(address, data);
+    }
+    else {
+        //throw std::exception("writing to not-deleted area");
+        throw WriteException();
+    }
+}
+
+void DeviceDriver::write_(long address, int data)
+{
     m_hardware->write(address, (unsigned char)data);
+}
+
+void DeviceDriver::setDevice(FlashMemoryDevice& hardware) 
+{
+    m_hardware = &hardware;
 }
--- device_driver_kata/device_driver.h
+++ device_driver_kata/device_driver.h
@@ -1,13 +1,37 @@
 #pragma once
 #include "flash_memory_device.h"
+#include <exception>
+
+class ReadException : public std::exception {
+public:
+    char const* what() const override {
+        return READ_EXCEPTION_MSG;
+    }
+    char const* READ_EXCEPTION_MSG = "Read verification failed; must show consistent values for five attempts";
+};
+
+class WriteException : public std::exception {
+public:
+    char const* what() const override {
+        return WRITE_EXCEPTION_MSG;
+    }
+    char const* WRITE_EXCEPTION_MSG = "Attempting to write to address with existing values; failing write";
+};
 
 class DeviceDriver
 {
 public:
+    DeviceDriver() = default;
     DeviceDriver(FlashMemoryDevice *hardware);
+    void setDevice(FlashMemoryDevice& hardware);
     int read(long address);
+    bool verifyRead(int readValue, long address);
     void write(long address, int data);
-
+    
+    static const int DELETED = FlashMemoryDevice::DELETED;
 protected:
     FlashMemoryDevice *m_hardware;
+
+    int read_(long address);
+    void write_(long address, int data);
 };
